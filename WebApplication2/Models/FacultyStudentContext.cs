bcb92ca2c218ccd8adc8a9d806d3d7a9a975c65f
Dell <Dell@DESKTOP-SCFRJ18>
2024-04-30 02:23:04 +0600

    Add project files.

---
+++ WebApplication2/Models/FacultyStudentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public class FacultyStudentContext : DbContext
+    {
+        public FacultyStudentContext(DbContextOptions<FacultyStudentContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
